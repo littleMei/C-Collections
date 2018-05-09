27332f28b207baa70971584e13aeb503f066b4a1
Danyi Luo <luodanyi0428@gmail.com>
2018-05-09 13:53:56 -0500

    Add List

---
+++ List/List/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                ID = 101,
+                Name = "Mark",
+                Salary = 5000,
+            };
+            Customer customer2 = new Customer
+            {
+                ID = 119,
+                Name = "John",
+                Salary = 3500
+            };
+            Customer customer3 = new Customer
+            {
+                ID = 110,
+                Name = "Pam",
+                Salary = 6500
+            };
+
+            List<Customer> customers = new List<Customer>(2); // List<> is strongly typed. This is type of Customer. 
+            customers.Add(customer1);
+            customers.Add(customer2);
+            customers.Add(customer3); // List can change the size
+            customers.Insert(0, customer3);
+
+            foreach(Customer c in customers)
+            {
+                Console.WriteLine(c.ID);
+            }
+
+            Console.WriteLine(customers.IndexOf(customer3)); // 0 is the result 
+
+            // the result will be 3
+            Console.WriteLine(customers.IndexOf(customer3, 1, 3));// the second parameter means which position you want start to search, 
+                                                                 // the third parameter means how many items you want to search
+                            
+            foreach(Customer c in customers)
+            {
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", c.ID, c.Name, c.Salary);
+            }
+
+            for (int i = 0; i < customers.Count; i++)
+            {
+                Customer c = customers[i];
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", c.ID, c.Name, c.Salary);
+            }
+
+            SavingsCustomer sr = new SavingsCustomer(); // SavingsCustomer is inherited from Customer class. 
+            customers.Add(sr); // So it is okay to add sr. it is still strongly typed. 
+
+
+            Console.WriteLine("-------------------------------------");
+
+            // check if customer is in the list 
+            if (customers.Contains(customer3))
+            {
+                Console.WriteLine("Customer 3 is in the list");
+            }
+            else
+            {
+                Console.WriteLine("Customer 3 is not in the list");
+            }
+
+            // use lambda expression
+            if (customers.Exists(cust => cust.Name.StartsWith("P")))
+            {
+                Console.WriteLine("Customer3 object exists in the list");
+            }
+            else
+            {
+                Console.WriteLine("Customer3 object does not exist in the list");
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
